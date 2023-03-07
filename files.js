aecf2e4af588a0154fccc1af6861eb771f3f65c0
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-07 10:17:02 +0530

    expofreadfile

--- files.js
+++ files.js
@@ -1,47 +1,48 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // reading files
-fs.readFile('./docs/blog.txt', (err, data) => {
+fs.readFile("./docs/blog.txt", (err, data) => {
   if (err) {
     console.log(err);
-  }  
+  }
+  // if only data then it is a buffer
   console.log(data.toString());
 });
 
 // console.log('last line');
 
 // writing files
-fs.writeFile('./docs/blog.txt', 'hello, world', () => {
-  console.log('file was written');
+fs.writeFile("./docs/blog.txt", "hello, world", () => {
+  console.log("file was written");
 });
 
-fs.writeFile('./docs/blog2.txt', 'hello, again', () => {
-  console.log('file was written');
+fs.writeFile("./docs/blog2.txt", "hello, again", () => {
+  console.log("file was written");
 });
 
 // directories
-if (!fs.existsSync('./assets')) {
-  fs.mkdir('./assets', err => {
+if (!fs.existsSync("./assets")) {
+  fs.mkdir("./assets", (err) => {
     if (err) {
       console.log(err);
     }
-    console.log('folder created');
+    console.log("folder created");
   });
 } else {
-  fs.rmdir('./assets', err => {
+  fs.rmdir("./assets", (err) => {
     if (err) {
       console.log(err);
     }
-    console.log('folder deleted');
+    console.log("folder deleted");
   });
 }
 
 // deleting files
-if (fs.existsSync('./docs/deleteme.txt')) {
-  fs.unlink('./docs/deleteme.txt', err => {
+if (fs.existsSync("./docs/deleteme.txt")) {
+  fs.unlink("./docs/deleteme.txt", (err) => {
     if (err) {
       console.log(err);
     }
-    console.log('file deleted');
+    console.log("file deleted");
   });
 }
