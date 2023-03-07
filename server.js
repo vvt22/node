d35ae277f493ac761d36f01c9d18bb43859e611f
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-07 18:48:20 +0530

    expoflocalhostandport

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  console.log("request made");
 });
 
-// localhost is the default value for 2nd argument
-server.listen(3000, 'localhost', () => {
-  console.log('listening for requests on port 3000');
+// localhost is the default value for 2nd argument which is like a domain name
+// points directly to our own computer
+// port no. are like door to our computer
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
 });
+
+//
+//
+//
+//
+// run this file to listen to request been made
