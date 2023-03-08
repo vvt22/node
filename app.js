e4f37f0857c6a1a846a10fafe9cb339baeec3f3b
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-08 09:12:26 +0530

    middlew

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 // express app
 const app = express();
@@ -8,49 +8,59 @@ const app = express();
 app.listen(3000);
 
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // middleware & static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+//LOGGING
 app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
+  console.log("new request made:");
+  console.log("host: ", req.hostname);
+  console.log("path: ", req.path);
+  console.log("method: ", req.method);
+  next(); //as the browser doesnt know what to do next after running USE
 });
 
 app.use((req, res, next) => {
-  console.log('in the next middleware');
+  console.log("in the next middleware");
   next();
 });
 
-app.use(morgan('dev'));
+app.use(morgan("dev")); //a third party package as logger
 
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
   ];
-  res.render('index', { title: 'Home', blogs });
+  res.render("index", { title: "Home", blogs });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
+  res.status(404).render("404", { title: "404" });
 });
